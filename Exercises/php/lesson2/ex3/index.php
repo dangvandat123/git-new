ee1fbdfe4dc18aa81ca056fb212b7f1ff5db6028
dangvandat123 <datdang431@example.com>
2021-03-10 23:41:17 +0700

    php-Exercises Signed-off-by: dangvandat123 <datdang431@example.com>

--- Exercises/php/lesson2/ex3/index.php
+++ Exercises/php/lesson2/ex3/index.php
@@ -1,10 +1,26 @@
-    <?php
-        $n=1;
-        $tong=0;
-        while ($n <= 100);
-        echo "Tổng của vòng lặp =".$tong +=$n;
-        "<br>"
+<?php
+    $n=1;
+    $i=20;
+    $tong=0;
+    $chia=0;
+    while ($n <= 100){
+        echo "$n,";
+        $tong += $n;
+        $n++;
         
-        
-        
-    ?>
+    } 
+    echo " Tổng = $tong" ."<br>";
+    while ($i >=20 and $i <= 50){
+        echo "$i,";
+        $chia +=$i;
+        $i++;
+       
+
+
+    }
+    if ($i % 3 ==0) {  
+        echo "các só chia hết cho 3 : $i";
+    }
+    
+    
+?>
--- Exercises/php/lesson2/ex4/index.php
+++ Exercises/php/lesson2/ex4/index.php
@@ -1,10 +1,5 @@
 <?php
-    $n=1;
-    $tong=0;
-    while ($n <= 100);
-    echo "Tổng của vòng lặp =".$n;
-    $n++;
-    "<br>"
+   
     
     
     
